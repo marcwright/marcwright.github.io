99fc15ad4a4de61fea274a990764abce33f177e2
marcwright <marc.wright@gmail.com>
2014-04-19 22:33:35 -0700

    added model citzen page and photos

--- app/js/app.js
+++ app/js/app.js
@@ -11,9 +11,29 @@ var app = angular.module('myApp', [
   'myApp.controllers'
   ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.when('/angular', {templateUrl: 'partials/angularIn60.html', controller: 'AngularIn60Ctrl'});
-  $routeProvider.when('/model', {templateUrl: 'partials/modelcitizen.html', controller: 'ModelCitizen'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider
+      .when('/view1', 
+        {
+          templateUrl: 'partials/partial1.html', 
+          controller: 'MyCtrl1'
+        })
+      .when('/view2', 
+        {
+          templateUrl: 'partials/partial2.html', 
+          controller: 'MyCtrl2'
+        })
+      .when('/angular', 
+        {
+          templateUrl: 'partials/angularIn60.html', 
+          controller: 'AngularIn60Ctrl'
+        })
+      .when('/model', 
+        {
+          templateUrl: 'partials/modelcitizen.html', 
+          controller: 'ModelCitizen'
+        })
+      .otherwise(
+        {
+          redirectTo: '/view1'
+        });
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,7 +10,6 @@ angular.module('myApp.controllers', [])
 		{letter:'b', leet:'|3'},
 		{letter:'c', leet:'('}
 		]
-
   	})
 
   .controller('MyCtrl2', function($scope) {
@@ -59,10 +58,41 @@ angular.module('myApp.controllers', [])
 
  .controller('ModelCitizen', function($scope) {
 
- 	$scope.names = [
-		{person:'Andrew', age:36}, 
-		{person:'Maren',age: 32},
-		{person:'Schmitty', age:56}]
+ 	$scope.songs = [
+		{title: 'THIS LOVE', artist: 'MAROON 5', singer: 'Keith', image: "img/keith.png"},
+		{title: 'GET BACK', artist: 'THE BEATLES', singer: 'Marc', image: "img/marc.png"},
+		{title: 'TAKIN IT TO THE STREETS', artist: 'DOOBIE BROTHERS', singer: 'Keith', image: "img/keith.png"},
+		{title: 'VERTIGO', artist: 'U2', singer: 'Marc', image: "img/marc.png"},
+		{title: 'TAKE ME TO THE PILOT', artist: 'ELTON JOHN', singer: 'Keith', image: "img/keith.png"}, 
+		{title: 'WHAT’S YOUR NAME', artist: 'LYNYRD SKYNAYRD', singer: 'Keith', image: "img/keith.png"},
+		{title: 'BOULEVARD BROKEN', artist: 'GREEN DAY', singer: 'Marc', image: "img/marc.png"},
+		{title: 'JUST WHAT I NEEDED', artist: 'CARS', singer: 'Keith', image: "img/keith.png"},
+		{title: 'BLUE MORNING', artist: 'FOREIGNER', singer: 'Marc', image: "img/marc.png"},
+		{title: 'HOLD THE LINE', artist: 'TOTO', singer: 'Marc', image: "img/marc.png"},
+		{title: 'JET', artist: 'WINGS', singer: 'Marc', image: "img/marc.png"},
+		{title: 'POWER OF LOVE', artist: 'HUEY LEWIS', singer: 'Keith', image: "img/keith.png"},
+		{title: 'ARE YA GONNA GO MY WAY', artist: 'LENNY KRAVITZ', singer: 'Marc', image: "img/marc.png"},
+		{title: 'LAST DANCE WITH MARY JANE', artist: 'TOM PETTY', singer: 'Keith', image: "img/keith.png"},
+		{title: 'I DON’T WANNA BE', artist: 'GAVIN DEGRAW', singer: 'Keith', image: "img/keith.png"},
+		{title: 'IT’S TIME', artist: 'IMAGINE DRAGONS', singer: 'Marc', image: "img/marc.png"},
+		{title: 'USE SOMEBODY', artist: 'KINGS OF LEON', singer: 'Marc', image: "img/marc.png"},
+		{title: 'HOWLIN’ FOR YOU', artist: 'BLACK KEYS', singer: 'Marc', image: "img/marc.png"},
+		{title: 'SARA SMILE', artist: 'HALL & OATES', singer: 'Keith', image: "img/keith.png"},
+		{title: 'RAMBLE ON', artist: 'LED ZEPPELIN', singer: 'Marc', image: "img/marc.png"},
+		{title: 'SATURDAY NITE’S ALRIGHT', artist: 'ELTON JOHN', singer: 'Marc', image: "img/marc.png"},
+		{title: 'YOU GET WHAT YOU GIVE', artist: 'NEW RADICALS', singer: 'Marc', image: "img/marc.png"},
+		{title: 'BLACK FRIDAY', artist: 'STEELY DAN', singer: 'Keith', image: "img/keith.png"},
+		{title: 'GET LUCKY', artist: 'DAFT PUNK', singer: 'Marc', image: "img/marc.png"},
+		{title: 'JUST LIKE HEAVEN', artist: 'THE CURE', singer: 'Marc', image: "img/marc.png"},
+		{title: 'LOCKED OUTTA HEAVEN', artist: 'BRUNO MARS', singer: 'Marc', image: "img/marc.png"},
+		{title: 'KISS ON MY LIST', artist: 'HALL & OATES', singer: 'Keith', image: "img/keith.png"},
+		{title: 'ROLLIN’ IN THE DEEP', artist: 'ADELE', singer: 'Marc', image: "img/marc.png"},
+		{title: 'LADY MADONNA', artist: 'THE BEATLES', singer: 'Marc', image: "img/marc.png"},
+		{title: 'YOU MAY BE RIGHT', artist: 'BILLY JOEL', singer: 'Keith', image: "img/keith.png"},
+		{title: 'KISS', artist: 'PRINCE', singer: 'Paul', image: "img/paul.png"},
+		{title: 'BEAST OF BURDEN', artist: 'THE ROLLING STONES', singer: 'Marc', image: "img/marc.png"},
+		{title: 'LEARN TO FLY', artist: 'FOO FIGHTERS', singer: 'Marc', image: "img/marc.png"},
+		{title: 'PANAMA', artist: 'VAN HALEN', singer: 'Marc', image: "img/marc.png"}]
 
   });
 
