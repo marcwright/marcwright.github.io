6aeccb572497e393ec56541bdb72d9c720ab7698
marcwright <marc.wright@gmail.com>
2014-04-19 20:00:48 -0700

    added model citizen page

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,19 @@
 'use strict';
 
-var app = angular.module('myApp', ['pascalprecht.translate']);
+// var app = angular.module('myApp', []);
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+var app = angular.module('myApp', [
   'ngRoute',
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
   'myApp.controllers'
-]).
+  ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.when('/angular', {templateUrl: 'partials/angularIn60.html', controller: 'AngularIn60Ctrl'});
+  $routeProvider.when('/model', {templateUrl: 'partials/modelcitizen.html', controller: 'ModelCitizen'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -13,11 +13,51 @@ angular.module('myApp.controllers', [])
 
   	})
 
-  .controller('MyCtrl2', function() {
+  .controller('MyCtrl2', function($scope) {
+
+  	$scope.kitties = [
+		{
+			catName: 'Karl',
+			monthsOld: 5,
+			gender: 'boy',
+			image: "img/carl.png",
+			adopted: false
+		},
+		{
+			catName: 'Jack',
+			monthsOld: 4,
+			gender: 'boy',
+			image: "img/jack.png",
+			adopted: false
+		},
+		{
+			catName: 'Oscar',
+			monthsOld: 2,
+			gender: 'boy',
+			image: "img/oscar.png",
+			adopted: false
+		},
+		{
+			catName: 'Princess Mew',
+			monthsOld: 3,
+			gender: 'girl',
+			image: "img/princessmew.png",
+			adopted: false
+		}
+	]
 
   })
 
  .controller('AngularIn60Ctrl', function($scope) {
+
+ 	$scope.names = [
+		{person:'Andrew', age:36}, 
+		{person:'Maren',age: 32},
+		{person:'Schmitty', age:56}]
+
+  })
+
+ .controller('ModelCitizen', function($scope) {
 
  	$scope.names = [
 		{person:'Andrew', age:36}, 
